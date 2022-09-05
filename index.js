fb3b76b14fa4eb90815b06000dc62dde4c9996ea
sabrinaMac <sabrina.scherma@gmail.com>
2022-09-05 16:19:42 -0600

    worked on index.html

---
+++ index.js
@@ -0,0 +1,6 @@
+const middle = document.getElementById('middlePara');
+const projects = document.getElementById('projects');
+
+projects.addEventListener('click', () => {
+    console.log('yep');
+})
