f4c850506f6d49f1aaf4a2943ad8ed94766eb499
sabrinaMac <sabrina.scherma@gmail.com>
2022-09-06 08:37:02 -0600

    set up javascript for index.html interactivity

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-const middle = document.getElementById('projectDivMiddle');
+const projectsMid = document.getElementById('projectDivMiddle');
 const projects = document.getElementById('projects');
 x = 0;
 
+const infoMid = document.getElementById('infoDivMiddle');
+const info = document.getElementById('info');
+y = 0;
+
 projects.addEventListener('click', () => {
-    console.log('yep');
     if (x == 0) {
-        middle.style.display = "block";
+        projectsMid.style.display = "block";
+        projectsMid.style.backgroundColor = "rgba(var(--frontColor), 0.2)"
         x++;
     } else if (x == 1) {
-        middle.style.display = "none";
+        projectsMid.style.display = "none";
         x--;
     }
-    
+});
+
+info.addEventListener('click', () => {
+    if (y == 0) {
+        infoMid.style.display = "block";
+        infoMid.style.backgroundColor = "rgba(var(--frontColor), 0.2)"
+        y++;
+    } else if (y == 1) {
+        infoMid.style.display = "none";
+        y--;
+    }
 })
+
