e511e27308f06dcd5b1e6b868744f320de4ef041
sabrinaMac <sabrina.scherma@gmail.com>
2022-09-06 00:43:40 -0600

    set up javascript and styling for index.html

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const middle = document.getElementById('middlePara');
+const middle = document.getElementById('projectDivMiddle');
 const projects = document.getElementById('projects');
+x = 0;
 
 projects.addEventListener('click', () => {
     console.log('yep');
+    if (x == 0) {
+        middle.style.display = "block";
+        x++;
+    } else if (x == 1) {
+        middle.style.display = "none";
+        x--;
+    }
+    
 })
