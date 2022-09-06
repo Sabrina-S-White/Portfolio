dec0e926c50f01157c155e7ae2c23b55e31ee356
sabrinaMac <sabrina.scherma@gmail.com>
2022-09-06 10:34:34 -0600

    finished animations for index.html

--- index.js
+++ index.js
@@ -6,12 +6,20 @@ const infoMid = document.getElementById('infoDivMiddle');
 const info = document.getElementById('info');
 y = 0;
 
+const contactMid = document.getElementById('contactDivMid');
+const contact = document.getElementById('contact');
+z = 0;
+
 projects.addEventListener('click', () => {
     if (x == 0) {
         infoMid.style.display = "none";
         y = 0;
+
+        contactMid.style.display = "none";
+        z = 0;
+
         projectsMid.style.display = "block";
-        projectsMid.style.backgroundColor = "rgba(var(--frontColor), 0.2)"
+        projectsMid.style.backgroundColor = "rgba(var(--frontColor), 0.2)";
         x++;
     } else if (x == 1) {
         projectsMid.style.display = "none";
@@ -23,12 +31,33 @@ info.addEventListener('click', () => {
     if (y == 0) {
         projectsMid.style.display = "none";
         x = 0;
+
+        contactMid.style.display = "none";
+        z = 0;
+
         infoMid.style.display = "block";
-        infoMid.style.backgroundColor = "rgba(var(--frontColor), 0.2)"
+        infoMid.style.backgroundColor = "rgba(var(--frontColor), 0.2)";
         y++;
     } else if (y == 1) {
         infoMid.style.display = "none";
         y--;
     }
-})
+});
+
+contact.addEventListener('click', () => {
+    if (z ==0) {
+        projectsMid.style.display = "none";
+        x = 0;
+
+        infoMid.style.display = "none";
+        y = 0;
+
+        contactMid.style.display = "block";
+        contactMid.style.backgroundColor = "rgba(var(--frontColor), 0.2)";
+        z++;
+    } else if (z == 1) {
+        contactMid.style.display = "none";
+        z--;
+    }
+});
 
